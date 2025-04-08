b7e313a6d5f64863915a79e8108578c0cdbd3777
manshu777 <79352371+Manshu777@users.noreply.github.com>
2025-04-08 16:19:07 +0530

    add-more-mess

--- app/Models/PopularDestination.php
+++ app/Models/PopularDestination.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Popular_Destination extends Model
+class PopularDestination extends Model
 {
     use HasFactory;
  protected $table="popular__destinations_flight";
